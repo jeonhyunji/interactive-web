b53a8a607b5995690535f57f1b8d89abbaf4b0a5
jeonhyunji <94jeonhyunji@gmail.com>
2021-01-27 22:47:36 +0900

    mouse down/move/up에 따라 card위치수정, rotation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,95 @@
 // import logo from './logo.svg';
 import './App.css';
 import Card from './card/Card';
+import { useState } from 'react';
+
+const radius = 400;
+const circleX = 100 + radius;
+const circleY = 200 + radius;
+const angle = 20;
 
 function App() {
+	const [moveAngle, setMoveAngle] = useState(0);
+	const [isMovable, setIsMovable] = useState(false);
+	const [fromXY, setFromXY] = useState({x: 0, y: 0});
 
-  const colors = ["red", "yellow", "green", "blue"];
-  var translateX = 0;
-  const cards = colors.map((value) => {
-    translateX += 100;
-    return <Card translateX={translateX} color={value} />
-  });
+	const n = Math.floor(360 / angle);
+	const cards = [];
+	var cardAngle = 90;
+	for (var i = 0; i < n; i++) {
+		var color;
+		if (i === 0) {
+			color = "red";
+		} else {
+			color = "green";
+			if (i % 2 === 1)
+				color = "green";
+			else
+				color = "yellow";
+		}
+		cards.push(
+			<Card color={color} angle={cardAngle + moveAngle} circleX={circleX} circleY={circleY} radius={radius}/>
+		);
+		cardAngle += angle;
+	}
 
-  return (
-    <div className="App">
-      {cards}
-    </div>
-  );
+	const onMouseDown = (ev) => {
+		setIsMovable(true);
+		setFromXY({
+			x: ev.clientX,
+			y: ev.clientY
+		});
+	};
+
+	const onMouseMove = (ev) => {
+		if (isMovable) {
+			var toX = ev.clientX;
+			var toY = ev.clientY;
+
+			var fromAngle = getAngle(fromXY.x, fromXY.y);
+			var toAngle = getAngle(toX, toY);
+
+			var move = 0;
+			// move right
+			if (toAngle > fromAngle) {
+				move = toAngle - fromAngle;
+			} else { // move left
+				move = - (fromAngle - toAngle);
+			}
+			setMoveAngle(moveAngle + move);
+			setFromXY({
+				x: ev.clientX,
+				y: ev.clientY
+			});
+		}
+	};
+
+	const onMouseUp = () => {
+		setIsMovable(false);
+	};
+
+
+	return (
+		<div className="App" onMouseDown={(ev) => onMouseDown(ev)} 
+			onMouseMove={(ev) => onMouseMove(ev)} 
+			onMouseUp={() => onMouseUp()}>
+			{/* <div>move: {moveAngle}</div> */}
+			{/* <div className="test-circle"></div> */}
+			{/* <div className="center"></div> */}
+			<div style={{display: 'inline-block', width: "1000px", height: "1000px", marginTop: "200px"}}>
+				{cards}
+			</div>
+		</div>
+	);
+}
+
+function getAngle(x, y) {
+	return radiansToDegrees(Math.atan2(circleY - y, x - circleX));
+}
+
+function radiansToDegrees(radians) {
+	var pi = Math.PI;
+	return radians * (180 / pi);
 }
 
 export default App;
--- src/card/Card.js
+++ src/card/Card.js
@@ -1,23 +1,51 @@
-import {useState} from 'react';
 import './Card.css';
+// import { useState } from 'react';
 
-function Card({color, translateX}) {
-    const [flag, setFlag] = useState(false);
+const cardWidth = 200;
+const cardHeight = 300;
 
-    const onMouseOver = () => setFlag(true);
-    const onMouseOut = () => setFlag(false);
+function Card({ color, angle, circleX, circleY, radius }) {
+    // const [focus, setFocus] = useState(false);
+
+    const posX = Math.floor(Math.cos(degreesToRadians(angle)) * radius);
+    const posY = - Math.floor(Math.sin(degreesToRadians(angle)) * radius);
+    const translateX = circleX + posX - (cardWidth / 2);
+    const translateY = circleY + posY - (cardHeight / 2);
+    // const rotateDeg = - (angle - 90);
+    const rotateDeg = 90 - angle;
+
+    // const scaleValue = (focus) ? 1.1 : 1;
+    const scaleValue = 1;
+    const transformValue = "translate3d(" + translateX + "px, "
+        + translateY + "px, 0px) rotate(" + rotateDeg + "deg) scale("
+        + scaleValue + ")";
+
+    // const onMouseOver = () => {
+    //     setFocus(true);
+    // };
+
+    // const onMouseOut = () => {
+    //     setFocus(false);
+    // };
 
     return (
-        <div className="card-items" 
+        <div className="card-items"
             style={{
-                backgroundColor:color, transform: "translate3d(" + translateX + "px, 100px, 0px)",
-                zIndex: flag ? 100 : 0,
-                scale: flag ? 1.2: 1
-            }}
-            onMouseOver={onMouseOver} onMouseOut={onMouseOut}>
+                backgroundColor: color, 
+                transform: transformValue,
+                // zIndex: (focus) ? 100 : 0
+                zIndex: ( 80 < angle%360 && angle%360 < 100) ? 100 : 0  
+            }}>
+            {/* {angle} */}
+            {/* onMouseOver={() => onMouseOver()} onMouseOut={() => onMouseOut()}> */}
         </div>
     );
-    
-  }
-  
-  export default Card;
+}
+
+
+function degreesToRadians(degrees) {
+    const pi = Math.PI;
+    return degrees * (pi / 180);
+}
+
+export default Card;
